b907949acb555c0a3b45a04731607c232e218cfb
amiericanderson <ganderson5@eagles.ewu.edu>
2021-05-19 21:10:09 -0700

    Initial Commit: CommandInterpreter with simple "Hello World" message.

---
+++ src/cs350s21project/cli/CommandInterpreter.java
@@ -0,0 +1,8 @@
+package cs350s21project.cli;
+
+public class CommandInterpreter {
+
+    public void evaluate(String command) throws RuntimeException {
+        System.out.println("hello world");
+    }
+}
