00c8bb7aa51a9310eb948b37f87e2152bc03b2cb
amiericanderson <ganderson5@eagles.ewu.edu>
2021-05-20 10:46:34 -0700

    Testing another commit from Gabe

--- src/cs350s21project/cli/CommandInterpreter.java
+++ src/cs350s21project/cli/CommandInterpreter.java
@@ -4,6 +4,6 @@ public class CommandInterpreter {
 
     public void evaluate(String command) throws RuntimeException {
         System.out.println("hello world");
-	System.out.println("this line is to test commit from teammate.")
+      System.out.println("this line is to test commit from gabe");
     }
 }
