9b693758968522b46ccebbcec9a8a5b6b67070fc
amiericanderson <ganderson5@eagles.ewu.edu>
2021-05-25 08:27:14 -0700

    Added all field variables uses to the top of class file as well as an import statement.

--- src/cs350s21project/cli/CommandInterpreter.java
+++ src/cs350s21project/cli/CommandInterpreter.java
@@ -1,11 +1,31 @@
 package cs350s21project.cli;
 
+import cs350s21project.datatype.*;
+
 public class CommandInterpreter {
+
+    Altitude altitude;
+    AttitudeYaw azimuth;
+    CoordinateWorld3D coordinates;
+    Course course;
+    DistanceNauticalMiles distance;
+    AttitudePitch elevation;
+    String filename;
+    FieldOfView fov;
+    AgentID id;
+    Latitude latitude;
+    Longitude longitude;
+    Power power;
+    Sensitivity sensitivity ;
+    int size;
+    Groundspeed speed;
+    Time time;
+
 
     public void evaluate(String command) throws RuntimeException {
         System.out.println("hello world");
-	System.out.println("this line is to test commit from teammate.");
-	System.out.println("testing 123");
-	System.out.println("cugel the clever");
+	    System.out.println("this line is to test commit from teammate.");
+	    System.out.println("testing 123");
+	    System.out.println("cugel the clever");
     }
 }
