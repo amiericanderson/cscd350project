44b4d70164099d745e1533617a66a28136f2f547
Pedro H <phernandez5@eagles.ewu.edu>
2021-05-25 09:24:42 -0700

    add basic skeleton of my methods, Pedro.

--- src/cs350s21project/cli/CommandInterpreter.java
+++ src/cs350s21project/cli/CommandInterpreter.java
@@ -28,4 +28,46 @@ public class CommandInterpreter {
 	    System.out.println("testing 123");
 	    System.out.println("cugel the clever");
     }
+
+    //define ship id1 with munition[s] (idn+)
+    public void CommandActorDefineShip(AgentID idn) {
+    }
+
+    //create actor id1 from id2 at coordinates with course course speed speed
+
+    //set id course course
+    public void CommandActorSetCourse(Course course) {
+	this.course = course;
+    }
+
+     //set id speed speed
+     public void CommandActorSetSpeed(Groundspeed speed) {
+	this.speed = speed;
+     }
+
+     //set id altitude|depth altitude
+     public void CommandActorSetAltitudeDepth(Altitude altitude) {
+	this.altitude = altitude;
+     }
+
+     //set id1 deploy munition id2
+     public void CommandActorDeployMunition(AgentID id2) {
+
+     }
+
+     //define sensor sonar active id with power power sensitivity sensitivity
+     public void CommandSensorDefineSonarActive(Sensitivity sensitivity) {
+
+     }
+
+     //define sensor sonar passive id with sensitivity sensitivity
+     public void CommandSensorDefineSonarPassive(Sensitivity sensitivity) {
+
+     }
+
+     //set update time
+     public void CommandMiscSetUpdate(Time time) {
+	this.time = time;
+     }
+
 }
