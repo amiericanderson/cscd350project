e788d1b3c3885655d5abbcf9af945c154f1f99ab
amiericanderson <ganderson5@eagles.ewu.edu>
2021-06-02 11:39:45 -0700

    Almost completed CommandViewCreateWindowTop command just missing manager

--- src/cs350s21project/cli/CommandInterpreter.java
+++ src/cs350s21project/cli/CommandInterpreter.java
@@ -1,7 +1,8 @@
 package cs350s21project.cli;
 
-import cs350s21project.controller.command.A_Command;
+import cs350s21project.controller.command.*;
 import cs350s21project.controller.command.misc.CommandMiscExit;
+import cs350s21project.controller.command.view.A_CommandViewCreate;
 import cs350s21project.controller.command.view.CommandViewCreateWindowTop;
 import cs350s21project.controller.command.view.CommandViewDeleteWindow;
 import cs350s21project.datatype.*;
@@ -26,7 +27,7 @@ public class CommandInterpreter {
     int size;
     Groundspeed speed;
     Time time;
-    A_Command theCommand;
+    static A_Command theCommand;
 
 
     public static class Create{
@@ -50,17 +51,63 @@ public class CommandInterpreter {
 			String[] words = cmd.split(" ");
 			AgentID id = new AgentID(words[2]);
 			int topViewSize = Integer.parseInt(words[6]);
+
 			String lat1 = words[7].substring(1);
+			String[] lat1Split1 = lat1.split("//*");
+			int lat1Degrees = Integer.parseInt(lat1Split1[0]);
+			String[] lat1Split2 = lat1Split1[1].split("//'");
+			int lat1Minutes = Integer.parseInt(lat1Split2[0]);
+			String[] lat1Split3 = lat1Split2[1].split("//");
+			double lat1Seconds = Double.valueOf(lat1Split3[0]);
+			Latitude latitude1 = new Latitude(lat1Degrees, lat1Minutes, lat1Seconds);
 
 			String lat2 = words[8];
+			String[] lat2Split1 = lat2.split("//*");
+			int lat2Degrees = Integer.parseInt(lat2Split1[0]);
+			String[] lat2Split2 = lat2Split1[1].split("//'");
+			int lat2Minutes = Integer.parseInt(lat2Split2[0]);
+			String[] lat2Split3 = lat2Split2[1].split("//");
+			double lat2Seconds = Double.valueOf(lat2Split3[0]);
+			Latitude latitude2 = new Latitude(lat2Degrees, lat2Minutes, lat2Seconds);
+
 			String lat3 = words[9].substring(0, words[9].length()-1);
+			String[] lat3Split1 = lat3.split("//*");
+			int lat3Degrees = Integer.parseInt(lat3Split1[0]);
+			String[] lat3Split2 = lat3Split1[1].split("//'");
+			int lat3Minutes = Integer.parseInt(lat3Split2[0]);
+			String[] lat3Split3 = lat3Split2[1].split("//");
+			double lat3Seconds = Double.valueOf(lat3Split3[0]);
+			Latitude latitude3 = new Latitude(lat3Degrees, lat3Minutes, lat3Seconds);
+
 
 			String long1 = words[10].substring(1);
+			String[] long1Split1 = long1.split("//*");
+			int long1Degrees = Integer.parseInt(long1Split1[0]);
+			String[] long1Split2 = long1Split1[1].split("//'");
+			int long1Minutes = Integer.parseInt(long1Split2[0]);
+			String[] long1Split3 = long1Split2[1].split("//");
+			double long1Seconds = Double.valueOf(long1Split3[0]);
+			Longitude longitude1 = new Longitude(long1Degrees, long1Minutes, long1Seconds);
+
 			String long2 = words[11];
+			String[] long2Split1 = long2.split("//*");
+			int long2Degrees = Integer.parseInt(long2Split1[0]);
+			String[] long2Split2 = long2Split1[1].split("//'");
+			int long2Minutes = Integer.parseInt(long2Split2[0]);
+			String[] long2Split3 = long2Split2[1].split("//");
+			double long2Seconds = Double.valueOf(long2Split3[0]);
+			Longitude longitude2 = new Longitude(long2Degrees, long2Minutes, long2Seconds);
+
 			String long3 = words[12].substring(0, words[12].length()-1);
+			String[] long3Split1 = long3.split("//*");
+			int long3Degrees = Integer.parseInt(long3Split1[0]);
+			String[] long3Split2 = long3Split1[1].split("//'");
+			int long3Minutes = Integer.parseInt(long3Split2[0]);
+			String[] long3Split3 = long3Split2[1].split("//");
+			double long3Seconds = Double.valueOf(long3Split3[0]);
+			Longitude longitude3 = new Longitude(long3Degrees, long3Minutes, long3Seconds);
 
-
-			//theCommand =  CommandViewCreateWindowTop()
+			theCommand = new CommandViewCreateWindowTop(null, cmd, id, topViewSize, latitude1, latitude2, latitude3, longitude1, longitude2, longitude3);
 			//Window window = new Window()
 
 		}
